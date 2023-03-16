0b4c7057262b1e5c4f7bdcff7ff6fbb532da844c
Klara12m <klaramarovic@gmail.com>
2023-03-16 17:45:43 +0100

    idk

---
+++ psi.lab1/psi.lab1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace psi.lab1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
